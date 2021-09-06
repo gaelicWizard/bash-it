7f60f5ad8fdccbacd10a4f9aa6e1526d7a80f0f3
John D Pell <John+git@gaelicWizard.net>
2021-09-06 00:09:19 -0700

    No need for `cat`
    The input redirection can go at the beginning, it doens't need to go at the end.

--- lint_clean_files.sh
+++ lint_clean_files.sh
@@ -5,10 +5,9 @@
 #  - Empty lines are ignored
 #  - Comment lines (#) are ignored
 #
-# shellcheck disable=SC2002  # Prefer 'cat' for cleaner script
 mapfile -t FILES < <(
-	cat clean_files.txt \
-		| grep -v -E '^\s*$' \
+	< clean_files.txt \
+		grep -v -E '^\s*$' \
 		| grep -v -E '^\s*#' \
 		| xargs -n1 -I{} find "{}" -type f
 )
