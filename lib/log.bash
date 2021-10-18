c6c893229235dd146b29fb1ee49decb4c5270743
John D Pell <John+git@gaelicWizard.net>
2021-10-18 14:36:42 -0400

    lib/log: adopt `logr`

--- lib/log.bash
+++ lib/log.bash
@@ -8,14 +8,10 @@ if [[ "${__bi_log_imported:-}" == "loaded" ]]; then
 fi
 __bi_log_imported="loaded"
 
-# Declare log severity levels, matching syslog numbering
-: "${BASH_IT_LOG_LEVEL_FATAL:=1}"
-: "${BASH_IT_LOG_LEVEL_ERROR:=3}"
-: "${BASH_IT_LOG_LEVEL_WARNING:=4}"
-: "${BASH_IT_LOG_LEVEL_ALL:=6}"
-: "${BASH_IT_LOG_LEVEL_INFO:=6}"
-: "${BASH_IT_LOG_LEVEL_TRACE:=7}"
-readonly "${!BASH_IT_LOG_LEVEL_@}"
+source ~/Projects/logr/logr.bash
+logr start -v "Bash It"
+((__logr_scope_depth--)) # minus one to account for `lib/log.bash`.
+#logr verbose
 
 declare -a __bash_it_log_prefix=("log" "${__bash_it_log_prefix[@]:-core}")
 
@@ -62,70 +58,22 @@ function _bash_it_log_prefix_push() {
 	_log_trace "Begin" "${component_name}"
 }
 
+alias _log_trace=': logr "DEBUG"'
+alias _log_debug='logr "INFO"'
+alias _log_warn='logr "WARN"'
+alias _log_error='logr "ERROR"'
+
 function _has_colors() {
 	# Check that stdout is a terminal, and that it has at least 8 colors.
 	[[ -t 1 && "${CLICOLOR:=$(tput colors 2> /dev/null)}" -ge 8 ]]
 }
 
-function _bash_it_log_message() {
-	about 'Internal function used for logging, uses __bash_it_log_prefix as a prefix'
-	param '1: color of the message'
-	param '2: severity of the message'
-	param '3: message to log'
-	group 'log'
-
-	message="($SECONDS) $2: ${__bash_it_log_prefix[1]:-}${__bash_it_log_prefix[1]:+: }${__bash_it_log_prefix[0]:-default}: $3"
-	_has_colors && echo -e "$1${message}${echo_normal:-}" || echo -e "${message}"
-}
-
-function _log_trace() {
-	[[ "${BASH_IT_LOG_LEVEL:-0}" -ge ${BASH_IT_LOG_LEVEL_TRACE?} ]] || return 0
-
-	about 'log a message by echoing to the screen. needs BASH_IT_LOG_LEVEL >= BASH_IT_LOG_LEVEL_TRACE'
-	param '1: message to log'
-	param '2: message origin'
-	example '$ _log_trace "Entering theme plugin"'
-	group 'log'
-
-	_bash_it_log_message "${echo_blue:-}" "TRACE" "$1${2:+ (}${2:-}${2:+)}"
-}
-
-function _log_debug() {
-	[[ "${BASH_IT_LOG_LEVEL:-0}" -ge ${BASH_IT_LOG_LEVEL_INFO?} ]] || return 0
-
-	about 'log a debug message by echoing to the screen. needs BASH_IT_LOG_LEVEL >= BASH_IT_LOG_LEVEL_INFO'
-	param '1: message to log'
-	example '$ _log_debug "Loading plugin git..."'
-	group 'log'
-
-	_bash_it_log_message "${echo_green:-}" "DEBUG" "$1"
-}
-
-function _log_warning() {
-	[[ "${BASH_IT_LOG_LEVEL:-0}" -ge ${BASH_IT_LOG_LEVEL_WARNING?} ]] || return 0
-
-	about 'log a message by echoing to the screen. needs BASH_IT_LOG_LEVEL >= BASH_IT_LOG_LEVEL_WARNING'
-	param '1: message to log'
-	example '$ _log_warning "git binary not found, disabling git plugin..."'
-	group 'log'
-
-	_bash_it_log_message "${echo_yellow:-}" " WARN" "$1"
-}
-
-function _log_error() {
-	[[ "${BASH_IT_LOG_LEVEL:-0}" -ge ${BASH_IT_LOG_LEVEL_ERROR?} ]] || return 0
-
-	about 'log a message by echoing to the screen. needs BASH_IT_LOG_LEVEL >= BASH_IT_LOG_LEVEL_ERROR'
-	param '1: message to log'
-	example '$ _log_error "Failed to load git plugin..."'
-	group 'log'
-
-	_bash_it_log_message "${echo_red:-}" "ERROR" "$1"
-}
-
 # Aliases have no scope, so we can manipulate the global environment.
 alias _log_clean_aliases_and_trap="trap - RETURN; unalias source . _log_clean_aliases_and_trap; _log_trace 'Log trace unregistered.'"
-_bash_it_library_finalize_hook+=('trap - RETURN' 'unalias source . _log_clean_aliases_and_trap' '_log_trace "Log trace unregistered."')
-alias source='_bash_it_log_prefix_push "${BASH_SOURCE##*/}" && builtin source'
+
+_bash_it_library_finalize_hook+=('trap - RETURN' 'unalias source . _log_clean_aliases_and_trap')
+
+alias source=': logr "TRACE" "Loading..." && builtin source'
 alias .=source
-trap _bash_it_log_prefix_pop RETURN
+
+trap ': logr "TRACE" "Loaded."' RETURN
