03c9616f3a591864000c6b70cc3d3c19ea6a1558
John D Pell <John+git@gaelicWizard.net>
2022-03-05 12:16:35 -0800

    themes: `shfmt` all the copies of `powerline.base`
    powerline sourcepath
    
    Themes/gitline: update `powerline.base` to match...?
    
    source

--- themes/powerline/powerline.theme.bash
+++ themes/powerline/powerline.theme.bash
@@ -1,8 +1,8 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2034 # Expected behavior for themes.
 
-# shellcheck source=../../themes/powerline/powerline.base.bash
-. "$BASH_IT/themes/powerline/powerline.base.bash"
+# shellcheck source-path=SCRIPTDIR/../powerline
+source "${BASH_IT?}/themes/powerline/powerline.base.bash"
 
 PROMPT_CHAR=${POWERLINE_PROMPT_CHAR:=""}
 POWERLINE_LEFT_SEPARATOR=${POWERLINE_LEFT_SEPARATOR:=""}
